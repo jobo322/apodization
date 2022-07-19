6e8cf3c787371187c10a6dd2101f26b6e698d6fc
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-07-19 05:47:29 -0500

    chore(applyWindows): change input parameter (work in progress)

--- src/applyWindow.ts
+++ src/applyWindow.ts
@@ -1,13 +1,22 @@
 import { DoubleArray } from "cheminfo-types";
 
-import { ExponentialOptions } from "./shapes/exponential";
-import { LorentzToGaussOptions } from "./shapes/lorentzToGauss";
 import { getData } from "./utils/getData";
 
-export type ShapeOptions = ExponentialOptions | LorentzToGaussOptions;
+interface GenericShapeOptions {
+  length?: number,
+  dw?: number,
+}
 
-export interface ApplyWindowOptions {
-  shape: ShapeOptions;
+export interface ApplyWindowOptions<T> {
+  /**
+   * acepts parameters a returns a function that returns the value of the window 
+   * function per index
+   */
+  func: (shape: T) => (i: number) => number;
+  /**
+   * options for func function
+   */
+  shape: T;
   /**
    * Start Location for Applying Apodize Window
    */
@@ -18,22 +27,20 @@ export interface ApplyWindowOptions {
    */
   pointsToShift?: number;
 }
-export type FunctionGenerator = (options: any) => (i: number) => number;
 
 /**
  * pure function that applies a window function to the input data.
  */
 export function applyWindow(
   data: DoubleArray,
-  func: FunctionGenerator,
-  options: ApplyWindowOptions
+  options: ApplyWindowOptions<GenericShapeOptions>
 ) {
-  const { shape: shapeOptions, start = 0, pointsToShift = 0 } = options;
+  const { func, shape: shapeOptions, start = 0, pointsToShift = 0 } = options;
 
   const dataLength = data.length;
 
-  let length = "length" in shapeOptions ? shapeOptions.length : data.length;
-  const windowData = getData(func, { ...options, length });
+  const { length = dataLength } = shapeOptions;
+  const windowData = getData(func({ ...options, length }), length);
 
   const result = new Float64Array(data);
   const firstEndPoint = Math.min(start + length, dataLength - pointsToShift);

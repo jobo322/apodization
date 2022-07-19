938542857f96131f562cdec15676f094686aebf6
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-07-19 06:17:12 -0500

    feat(applyWindow)!: change input parameter
    accepts an function that returns directly the window function value

--- src/applyWindow.ts
+++ src/applyWindow.ts
@@ -3,29 +3,32 @@ import { DoubleArray } from "cheminfo-types";
 import { getData } from "./utils/getData";
 
 interface GenericShapeOptions {
-  length?: number,
-  dw?: number,
+  length?: number;
+  dw?: number;
 }
 
 export interface ApplyWindowOptions<T> {
   /**
-   * acepts parameters a returns a function that returns the value of the window 
-   * function per index
+   * Function that returns the value of the window function per index
    */
-  func: (shape: T) => (i: number) => number;
-  /**
-   * options for func function
-   */
-  shape: T;
+  func: (i: number) => number;
   /**
    * Start Location for Applying Apodize Window
    */
   start?: number;
   /**
+   * length of the window function
+   */
+  length?: number;
+  /**
    * number of points at the end of the shape to apply the window function inverted
    * @default 0
    */
   pointsToShift?: number;
+  /**
+   * output array
+   */
+  output?: DoubleArray;
 }
 
 /**
@@ -35,22 +38,26 @@ export function applyWindow(
   data: DoubleArray,
   options: ApplyWindowOptions<GenericShapeOptions>
 ) {
-  const { func, shape: shapeOptions, start = 0, pointsToShift = 0 } = options;
-
   const dataLength = data.length;
 
-  const { length = dataLength } = shapeOptions;
-  const windowData = getData(func({ ...options, length }), length);
+  const {
+    func,
+    start = 0,
+    length = dataLength,
+    pointsToShift = 0,
+    output = new Float64Array(data),
+  } = options;
 
-  const result = new Float64Array(data);
+  const windowData = getData(func, length);
+
   const firstEndPoint = Math.min(start + length, dataLength - pointsToShift);
   for (let i = start, j = 0; i < firstEndPoint; i++) {
-    result[i] *= windowData[j++];
+    output[i] *= windowData[j++];
   }
 
   for (let i = dataLength - 1, j = 0; i > dataLength - pointsToShift - 1; i--) {
-    result[i] *= windowData[j++];
+    output[i] *= windowData[j++];
   }
 
-  return result;
+  return output;
 }
